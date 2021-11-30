55a85c80cf060d301a3f21f03f70faa53949cacb
Tanuj Singh <singh.tanuj021091@gmail.com>
2021-11-30 15:09:05 +0530

    commit-2

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    letterSpacing: {
+      widest: '0.5em',
+     },
+
+     screens: {
+      'xs': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
